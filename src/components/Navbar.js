a6849df1f3445fb28d62bced2850702812cc3112
leonelaam <77633649+Leonelaam@users.noreply.github.com>
2021-09-20 22:37:24 -0600

    pendiente home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,22 +7,21 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
     return ( 
         <>
-    <nav className="navbar navbar-expand-lg bg-info mb-2 ml-5">
-    <Link to="/">Kino</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav className="navbar navbar-expand-lg bg-info mb-2 ml-5">  
+    <strong><Link className="navbar-brand mx-5" to="/">Kino</Link></strong>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-    
       <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
         <ul className="navbar-nav p-2">
-          <li className="nav-item active mx-3">
-            <Link to="/pelis">Peliculas</Link>
+          <li className="nav-item active mx-4">
+          <strong><Link to="/pelis">Peliculas</Link></strong>
           </li>
-          <li className="nav-item mx-3">
-          <Link to="/progtv">Programas de television</Link>
+          <li className="nav-item mx-4">
+          <strong><Link to="/person">Personas</Link></strong>
           </li>
-          <li className="nav-item mx-3">
-          <Link to="/person">Personas</Link>
+          <li className="nav-item mx-4">
+          <strong><Link to="/progTV">Programas TV</Link></strong>
           </li>
         </ul>
       </div>
